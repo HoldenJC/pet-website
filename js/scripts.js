e2e46c7cf88a71634c8b4812763db3a443ed629d
Holden Clark and Na Hyung Choi <holden@>
2019-06-04 10:41:09 -0700

    add light / dark theme buttons, add highlightable paragraph

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(function () {
+  $("button#light").click(function(){
+    $("body").removeClass();
+    $("body").addClass("light-bg");
+  });
+
+  $("button#dark").click(function(){
+    $("body").removeClass();
+    $("body").addClass("dark-bg");
+  });
+
+  $(".highlightable").click(function(){
+    $(".highlightable").addClass("highlightBox");
+  });
+});
